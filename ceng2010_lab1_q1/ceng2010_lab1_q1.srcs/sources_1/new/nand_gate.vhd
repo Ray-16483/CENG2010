178eb058dee9cb32cd014854e3fc33ab1acd1656
Ray-16483 <124970412+Ray-16483@users.noreply.github.com>
2023-02-10 14:06:33 +0800

    Add files via upload

---
+++ ceng2010_lab1_q1/ceng2010_lab1_q1.srcs/sources_1/new/nand_gate.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2022/09/23 21:11:52
+-- Design Name: 
+-- Module Name: nand_gate - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity nand_gate is
+    Port ( sw0 : in STD_LOGIC;
+           sw1 : in STD_LOGIC;
+           sw2 : in STD_LOGIC;
+           sw3 : in STD_LOGIC;
+           led0 : out STD_LOGIC);
+end nand_gate;
+
+architecture Behavioral of nand_gate is
+
+begin
+
+led0 <= (sw0 xor sw1) and (sw2 nor sw3);
+
+end Behavioral;
